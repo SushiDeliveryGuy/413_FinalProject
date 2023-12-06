6d8f5e759686d2666118b81a4ff82674410a31cf
SushiDeliveryGuy <112435954+SushiDeliveryGuy@users.noreply.github.com>
2023-12-06 13:20:48 -0700

    account.html and index

--- account.js
+++ account.js
@@ -1,8 +1,18 @@
 // public/javascripts/account.js
 $(function (){
-    $('#logout').on('click', function() {
-        window.location.replace("index.html");
+    $('#registerDevice').on('click', function() {
+        // TODO: registering the device and saving the name into "ID"
+
+        deviceOptions(ID);
     });
+
+    $('deviceID').onChange(/*TODO: function to update Weekly and Daily View for device*/);
+    $('calendar').onChange(/*TODO: function to update Weekly and Daily view for date change*/);
+
+
+
+
+    $('#logout').click(logout);
     
 });
 
@@ -11,5 +21,7 @@ function logout() {
     window.location.replace("index.html");
 }
 
-
-
+function deviceOptions(deviceName) {
+    let list = $('#deviceID');
+    list.innerHTML += "<option>" + deviceName + "</option>";
+}
