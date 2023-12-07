a73dce2ceaa2871fb0aeedeb3336cbc3144f6333
MikeNikeCode <117119939+MikeNikeCode@users.noreply.github.com>
2023-12-06 19:18:56 -0700

    Update account.js for change password

--- account.js
+++ account.js
@@ -25,3 +25,40 @@ function deviceOptions(deviceName) {
     let list = $('#deviceID');
     list.innerHTML += "<option>" + deviceName + "</option>";
 }
+
+function changePassword() {
+    var username = document.getElementById("username").value;
+    var currentPassword = document.getElementById("currentPassword").value;
+    var newPassword = document.getElementById("newPassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    // Client-side validation
+    if (newPassword !== confirmPassword) {
+        alert("New password and confirm password do not match.");
+        return;
+    }
+
+    // AJAX request to the server for password change
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/users/changePassword", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                alert("Password changed successfully");
+            } else {
+                alert("Error changing password. Please try again.");
+            }
+        }
+    };
+
+    // Sending the request with the username, current password, and new password
+    var data = JSON.stringify({
+        username: username,
+        currentPassword: currentPassword,
+        newPassword: newPassword
+    });
+
+    xhr.send(data);
+}
