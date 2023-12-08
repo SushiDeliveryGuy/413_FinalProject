3eee1d7f00feddab2fd8edc4ca2874d3d90e4c18
SushiDeliveryGuy <s4k3@arizona.edu>
2023-12-07 22:33:08 -0700

    nav bar

--- account.js
+++ account.js
@@ -62,3 +62,13 @@ function changePassword() {
 
     xhr.send(data);
 }
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
